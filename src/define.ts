e7da9b13dae107f1961d7758a3d5958cbaaf0871
Kellan Corbitt <13971870+Kbitt@users.noreply.github.com>
2021-05-03 21:44:45 -0700

    update api-extractor

--- src/define.ts
+++ src/define.ts
@@ -247,9 +247,8 @@ export type UseStore<T> = {
 
 /**
  * Define a store, returning its use hook.
- * @param objectOrConstructor
- * The store configuration. Can be an object composed of regular POJO of properties, methods, getters and setters, or a function or constructor returning an object
- * @param options
+ * @param objectOrConstructor - The store configuration. Can be an object composed of regular POJO of properties, methods, getters and setters, or a function or constructor returning an object
+ * @param options - store config options
  * @returns a hook function which returns the store instance.
  */
 export function defineStore<T extends { new (): {} } | (() => {}) | {}>(
