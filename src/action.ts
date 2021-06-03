06ba87d5776e9ceca52575baf97b49add50a5ec6
Kellan Corbitt <13971870+Kbitt@users.noreply.github.com>
2021-06-03 00:24:28 -0700

    don't register actions in store

--- src/action.ts
+++ src/action.ts
@@ -6,17 +6,5 @@ export const defineAction = (
   action: (...args: any) => any,
   options?: VuexOptions
 ) => {
-  if (!options) {
-    return action
-  }
-
-  if (!options.module.actions) {
-    options.module.actions = {}
-  }
-
-  options.module.actions[key] = (_, payload) => {
-    return action(...payload)
-  }
-
-  return (...args: any) => options.getStore().dispatch(`${id}/${key}`, args)
+  return action
 }
